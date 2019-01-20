bba8ab47f70bc00a725b05dc6f93506e84a031e4
crazypeace <665889+crazypeace@users.noreply.github.com>
2019-01-21 05:12:50 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 "use strict";
 const $ = selector => document.querySelector(selector);
 const NUM_PHRASE = 4;
-let dict = null;
+let dictpinyin = null;
+let dictwubi = null;
 
 async function loadDict(url) {
   const resp = await fetch(url);
@@ -9,31 +10,34 @@ async function loadDict(url) {
   return text.split('\n')
     .filter(s => s.includes('\t'))
     .map(s => s.split('\t'))
-    .map(([hans, pinyin]) => ({
+    .map(([hans, code]) => ({
+      code: code.trim(),
       hans: hans.trim(),
-      pinyin: pinyin.trim(),
     }));
 }
 
 (async function () {
   try {
-    dict = await loadDict("dict.txt");
+    dictpinyin = await loadDict("pinyin8k.wordlist");
+	dictwubi = await loadDict("wubi8k.wordlist");
   } catch (e) {
     alert("Fail to load dict: " + e);
     return;
   }
-  console.log(`${dict.length} phrases loaded`);
-  document.querySelectorAll('.num-phrases')
-    .forEach(elm => elm.textContent = `${dict.length}`);
+  console.log(`dictpinyin${dictpinyin.length} phrases loaded`);
+  console.log(`dictwubi${dictwubi.length} phrases loaded`);
   generatePassphrase();
   $('#generator').classList.remove('loading');
   $('#generate').disabled = false;
 })();
 
 function generatePassphrase() {
-  const list = $('#phrases');
-  list.innerHTML = '';
+  const listpinyin = $('#phrasespinyin');
+  listpinyin.innerHTML = '';
+ const listwubi = $('#phraseswubi');
+ listwubi.innerHTML = '';
   let randoms = new Uint16Array(NUM_PHRASE);
+  
   if (document.getElementById("randomarray").value == "")
   {
     window.crypto.getRandomValues(randoms);
@@ -43,17 +47,22 @@ function generatePassphrase() {
     var randomarray = document.getElementById("randomarray").value;
     randoms = randomarray.split('\t', 4);
   }
+  
   Array.from(randoms)
-  .map(n => dict[n % dict.length])
+  .map(n => dictpinyin[n % dictpinyin.length])
   .map(phraseToHTML)
-  .forEach(html => list.appendChild(html));
+  .forEach(html => listpinyin.appendChild(html));
+  Array.from(randoms)
+  .map(n => dictwubi[n % dictwubi.length])
+  .map(phraseToHTML)
+  .forEach(html => listwubi.appendChild(html)); 
 }
 
 function phraseToHTML(phrase) {
   const template = $('#phrase');
   const content = document.importNode(template, true).content;
   const li = content.querySelector('li');
-  li.textContent = phrase.pinyin;
+  li.textContent = phrase.code;
   li.dataset.hans = phrase.hans;
   return content;
 }
